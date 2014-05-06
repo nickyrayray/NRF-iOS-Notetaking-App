06fc121971dbb1349f6781437cd468be1a8d18e9
nikoris77 <nfalba@gmail.com>
2014-05-06 10:26:44 -0400

    Created a Note class

---
+++ NotePad/NRFNote.h
@@ -0,0 +1,17 @@
+//
+//  NRFNote.h
+//  NotePad
+//
+//  Created by Nicholas Falba on 5/6/14.
+//  Copyright (c) 2014 Nicholas Falba. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NRFNote : NSObject
+
+@property (nonatomic) NSString *content;
+@property (nonatomic) NSString *title;
+@property (nonatomic) NSDate *time;
+
+@end
---
+++ NotePad/NRFNote.m
@@ -0,0 +1,14 @@
+//
+//  NRFNote.m
+//  NotePad
+//
+//  Created by Nicholas Falba on 5/6/14.
+//  Copyright (c) 2014 Nicholas Falba. All rights reserved.
+//
+
+#import "NRFNote.h"
+
+@implementation NRFNote
+
+
+@end
