6a9d87c32f1b6e5d69b4ff1f8c97785279c10f05
nikoris77 <nfalba@gmail.com>
2014-05-05 21:46:06 -0400

    Initial Commit

---
+++ NotePad/NRFAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  NRFAppDelegate.h
+//  NotePad
+//
+//  Created by Nicholas Falba on 5/5/14.
+//  Copyright (c) 2014 Nicholas Falba. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NRFAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ NotePad/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  NotePad
+//
+//  Created by Nicholas Falba on 5/5/14.
+//  Copyright (c) 2014 Nicholas Falba. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "NRFAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([NRFAppDelegate class]));
+    }
+}
